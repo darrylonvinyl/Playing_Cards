db6821aa1deb32d86cb17330c09e2cb4f7ddf5c7
darryl <dawasum2@gmail.com>
2022-10-23 09:12:47 -0600

    Initial draft of card/deck classes

---
+++ playing_cards.py
@@ -0,0 +1,19 @@
+from random import shuffle
+
+"""This module contains a deck and a card class to simulate real playing 
+cards."""
+
+class Card:
+    card_dict = {2:"2", 3:"3", 4:"4", 5:"5", 6:"6", 7:"7", 8: "8", 9:"9",
+                    10:"10", 11:"Jack", 12:"Queen", 13:"King", 14:"Ace"}
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+        self.name = card_dict[value]
+    
+    def __str__(self):
+        return f"{self.name} of {self.suit}"
+    
+
+class Deck:
+    pass
